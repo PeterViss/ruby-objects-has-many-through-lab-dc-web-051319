26578c744b767c32d2cb4044873418ccde8e51d3
Peter Viss <peter.c.viss@biola.edu>
2019-05-18 15:02:55 -0400

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,33 @@
+class Doctor
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(patient, self, date)
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+       appointment.doctor == self
+    end
+  end
+
+  def patients
+    Appointment.all.collect do |appt|
+      if appt.doctor == self
+        appt.patient
+      end
+    end
+  end
+
+end
